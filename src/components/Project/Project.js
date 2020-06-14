c724747302e9138351df3f8f51a0fcf3e961cd38
Tcaprioli <Capriolityler@gmail.com>
2020-06-14 15:58:54 -0400

    added project icons

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -28,10 +28,10 @@ class Project extends React.Component {
           <Col>
               <div className="details">
                   {/* <p> Expectations: {this.props.project.expectations} </p> */}
-                  <p> <img src={companyImg} /> {this.props.project.organization.name} </p>
-                  <p> <img src={timelineImg} /> {this.props.project.timeline} </p>
-                  <p> <img src={teamImg} /> {this.props.project.team_size} </p>
-                  <p> <img src={skillsImg} /> {this.props.project.tech_stack} </p>
+                  <p> <img className="icon-pro" src={companyImg} /> {this.props.project.organization.name} </p>
+                  <p> <img className="icon-pro" src={timelineImg} /> {this.props.project.timeline} </p>
+                  <p> <img className="icon-pro" src={teamImg} /> {this.props.project.team_size} </p>
+                  <p> <img className="icon-pro" src={skillsImg} /> {this.props.project.tech_stack} </p>
               </div>
            </Col>
         </Row>
