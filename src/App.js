8e3c2cbd3fc1ada94e5fb144b365c63f63adf074
Tcaprioli <Capriolityler@gmail.com>
2020-06-14 12:03:19 -0400

    home page and search page styled

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Form from './components/Form'
 import Project from './components/project'
 import Home from './components/home'
+import ProjectIndex from './components/project-index'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 
@@ -13,6 +15,7 @@ function App() {
       <Form/> */}
       {/* <Project/> */}
       <Home/>
+      {/* <ProjectIndex/> */}
     </div>
   );
 }
